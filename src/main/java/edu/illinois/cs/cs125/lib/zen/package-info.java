4b655f9aa483cf7dfca5a372f87520756f5a2a3c
Geoffrey Challen <challen@illinois.edu>
2017-09-04 15:27:59 -0500

    New JavaDocs with explicit version.

--- src/main/java/edu/illinois/cs/cs125/lib/zen/package-info.java
+++ src/main/java/edu/illinois/cs/cs125/lib/zen/package-info.java
@@ -1,10 +1,11 @@
 /**
  * Zen: A Simple Java Graphics Library
  *
- * Originally written by Lawrence Angrave, later extended by Sasha Jaffarove, and cleaned up,
- * recovered to its original state, and published by Geoffrey Challen.
+ * Originally written by Lawrence Angrave, later extended by Sasha Jaffarove,
+ * and cleaned up, recovered to its original state, and published by Geoffrey
+ * Challen. Version 1.0.0 published on Sep 4, 2017 by Geoffrey Challen.
  *
- * @since 0.1
- * @version 1.0
+ * @see <a href="https://github.com/cs125-illinois/Zen">Zen on Github</a>
+ * @see <a href="https://cs125-illinois.github.io/Zen/">Zen Documentatino</a>
  */
 package edu.illinois.cs.cs125.lib.zen;
