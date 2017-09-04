140a1992513a93d06819c133012d7f1d8b01288d
Geoffrey Challen <challen@illinois.edu>
2017-09-04 07:36:02 -0500

    Continuing cleanup.

--- src/main/java/edu/illinois/cs/cs125/lib/zen/ZenSprite.java
+++ src/main/java/edu/illinois/cs/cs125/lib/zen/ZenSprite.java
@@ -1,37 +1,76 @@
 package edu.illinois.cs.cs125.lib.zen;
 
+/**
+ * Zen sprite class.
+ */
 public abstract class ZenSprite extends Point {
 
-    private int layer = 1;
+    /** My layer. */
+    private int myLayer = 1;
 
+    /**
+     * Create a new sprite at (0, 0).
+     */
     public ZenSprite() {
         super();
     }
 
+    /**
+     * Create a new sprite at integer coordinates.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     */
     public ZenSprite(final int x, final int y) {
         super(x, y);
     }
 
+    /**
+     * Create a new sprite at floating point coordinates.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     */
     public ZenSprite(final double x, final double y) {
         super(x, y);
     }
 
+    /**
+     * Draw.
+     */
     public abstract void draw();
+
+    /**
+     * Move.
+     */
     public abstract void move();
 
     /**
      * Returns the distance between this sprite and another sprite.
+     *
+     * @param other the other sprite
+     * @return the distance between the two sprites
      */
     public double distanceTo(final ZenSprite other) {
-        return Math.sqrt(Math.pow(this.rawX() - other.rawX(), 2)
-                + Math.pow(this.rawY() - other.rawY(), 2));
+        return Math.sqrt(
+                Math.pow(this.rawX() - other.rawX(), 2) + Math.pow(this.rawY() - other.rawY(), 2));
     }
 
+    /**
+     * Gets the layer.
+     *
+     * @return the layer
+     */
     public final int getLayer() {
-        return layer;
+        return myLayer;
     }
 
+    /**
+     * Sets the layer.
+     *
+     * @param layer the new layer
+     */
     public final void setLayer(final int layer) {
-        this.layer = layer;
+        this.myLayer = layer;
     }
 }
