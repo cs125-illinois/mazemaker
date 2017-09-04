ac6cf09f0e6c618fa99850e843513c372f83ecca
Geoffrey Challen <challen@illinois.edu>
2017-09-04 15:33:35 -0500

    Documentation fix.

--- src/main/java/edu/illinois/cs/cs125/lib/zen/package-info.java
+++ src/main/java/edu/illinois/cs/cs125/lib/zen/package-info.java
@@ -3,9 +3,9 @@
  *
  * Originally written by Lawrence Angrave, later extended by Sasha Jaffarove,
  * and cleaned up, recovered to its original state, and published by Geoffrey
- * Challen. Version 1.0.0 published on Sep 4, 2017 by Geoffrey Challen.
+ * Challen. Version 1.0.1 published on Sep 4, 2017 by Geoffrey Challen.
  *
  * @see <a href="https://github.com/cs125-illinois/Zen">Zen on Github</a>
- * @see <a href="https://cs125-illinois.github.io/Zen/">Zen Documentatino</a>
+ * @see <a href="https://cs125-illinois.github.io/Zen/">Zen Documentation</a>
  */
 package edu.illinois.cs.cs125.lib.zen;
