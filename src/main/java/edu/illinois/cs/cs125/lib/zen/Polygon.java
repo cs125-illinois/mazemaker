68f589e19afa809c4d6dc57610f959164c0687f4
Geoffrey Challen <challen@illinois.edu>
2017-09-04 07:17:56 -0500

    Continuing cleanup.

--- src/main/java/edu/illinois/cs/cs125/lib/zen/Polygon.java
+++ src/main/java/edu/illinois/cs/cs125/lib/zen/Polygon.java
@@ -1,113 +1,218 @@
 package edu.illinois.cs.cs125.lib.zen;
 
+/**
+ * Zen polygon class.
+ */
 public class Polygon extends ZenShape {
-    private int[] x, y;
 
+    /** My X and Y coordinates. */
+    private int[] myX, myY;
+
+    /**
+     * Draw a new black polygon at (0, 0) from an array of points.
+     *
+     * @param points the array of points.
+     */
     public Polygon(final Point... points) {
         this(0, 0, null, points);
     }
 
+    /**
+     * Draw a new polygon at (0, 0).
+     *
+     * @param color the color
+     * @param points the array of points
+     */
     public Polygon(final String color, final Point... points) {
         this(0, 0, color, points);
     }
 
-    public Polygon(final int xpos, final int ypos, final Point... points) {
-        this(xpos, ypos, null, points);
+    /**
+     * Draw a new black polygon.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     * @param points the array of points
+     */
+    public Polygon(final int x, final int y, final Point... points) {
+        this(x, y, null, points);
     }
 
-    public Polygon(final int xpos, final int ypos, final String color,
-            final Point... points) {
+    /**
+     * Draw a new polygon.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     * @param color the color
+     * @param points the array of points
+     */
+    public Polygon(final int x, final int y, final String color, final Point... points) {
         this.setColor(color);
-        x = new int[points.length];
-        y = new int[points.length];
+        myX = new int[points.length];
+        myY = new int[points.length];
         for (int i = 0; i < points.length; i++) {
-            x[i] = points[i].getX();
-            y[i] = points[i].getY();
+            myX[i] = points[i].getX();
+            myY[i] = points[i].getY();
         }
-        setX(xpos);
-        setY(ypos);
+        setX(x);
+        setY(y);
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.ZenShape#draw()
+     */
     @Override
     public final void draw() {
-        if (x != null && x.length > 0) {
-            Zen.fillPolygon(x, y);
+        if (myX != null && myX.length > 0) {
+            Zen.fillPolygon(myX, myY);
         }
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#set(int, int)
+     */
     @Override
     public final void set(final int x, final int y) {
         change(x - this.getX(), y - this.getY());
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#set(double, double)
+     */
     @Override
     public final void set(final double x, final double y) {
         change(x - this.getX(), y - this.getY());
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#setX(int)
+     */
+    @Override
     public final void setX(final int x) {
         changeX(x - this.getX());
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#setX(double)
+     */
+    @Override
     public final void setX(final double x) {
         changeX(x - this.getX());
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#setY(int)
+     */
+    @Override
     public final void setY(final int y) {
         changeY(y - this.getY());
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#setY(double)
+     */
+    @Override
     public final void setY(final double y) {
         changeY(y - this.getY());
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#change(int, int)
+     */
     @Override
     public final void change(final int dx, final int dy) {
-        for (int i = 0; i < x.length; i++) {
-            x[i] += dx;
-            y[i] += dy;
+        for (int i = 0; i < myX.length; i++) {
+            myX[i] += dx;
+            myY[i] += dy;
         }
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#change(double, double)
+     */
     @Override
     public final void change(final double dx, final double dy) {
-        for (int i = 0; i < x.length; i++) {
-            x[i] += dx;
-            y[i] += dy;
+        for (int i = 0; i < myX.length; i++) {
+            myX[i] += dx;
+            myY[i] += dy;
         }
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#changeX(int)
+     */
     @Override
     public final void changeX(final int amount) {
-        for (int i = 0; i < x.length; i++) {
-            x[i] += amount;
+        for (int i = 0; i < myX.length; i++) {
+            myX[i] += amount;
         }
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#changeY(int)
+     */
     @Override
     public final void changeY(final int amount) {
-        for (int i = 0; i < y.length; i++) {
-            y[i] += amount;
+        for (int i = 0; i < myY.length; i++) {
+            myY[i] += amount;
         }
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#changeX(double)
+     */
     @Override
     public final void changeX(final double amount) {
-        for (int i = 0; i < x.length; i++) {
-            x[i] += amount;
+        for (int i = 0; i < myX.length; i++) {
+            myX[i] += amount;
         }
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see edu.illinois.cs.cs125.lib.zen.Point#changeY(double)
+     */
     @Override
     public final void changeY(final double amount) {
-        for (int i = 0; i < y.length; i++) {
-            y[i] += amount;
+        for (int i = 0; i < myY.length; i++) {
+            myY[i] += amount;
         }
     }
 
+    /**
+     * Get a point in the polygon by index.
+     *
+     * @param index the index of the point to return
+     * @return the point
+     */
     public final Point getPoint(final int index) {
-        if (index >= 0 && index < x.length) {
-            return new Point(x[index], y[index]);
+        if (index >= 0 && index < myX.length) {
+            return new Point(myX[index], myY[index]);
         }
         return null;
     }
