9e8ba2eb081ff9f11b6428af5ebac98ecb7b4ebf
Geoffrey Challen <challen@illinois.edu>
2017-09-04 07:37:46 -0500

    Continuing cleanup.

--- src/main/java/edu/illinois/cs/cs125/lib/zen/ZenShape.java
+++ src/main/java/edu/illinois/cs/cs125/lib/zen/ZenShape.java
@@ -1,34 +1,70 @@
 package edu.illinois.cs.cs125.lib.zen;
 
+/**
+ * Zen shape class.
+ */
 public abstract class ZenShape extends Point {
-    private String color;
 
+    /** My color. */
+    private String myColor;
+
+    /**
+     * Create a new shape at (0, 0).
+     */
     public ZenShape() {
         super(0, 0);
     }
 
+    /**
+     * Create a new shape at integer coordinates.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     */
     public ZenShape(final int x, final int y) {
         super(x, y);
     }
 
+    /**
+     * Create a new shape at floating point coordinates.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     */
     public ZenShape(final double x, final double y) {
         super(x, y);
     }
 
+    /**
+     * Draw.
+     */
     public abstract void draw();
 
+    /**
+     * Color and draw.
+     */
     public final void colorAndDraw() {
-        if (color != null) {
-            Zen.setColor(color);
+        if (myColor != null) {
+            Zen.setColor(myColor);
         }
         this.draw();
     }
 
+    /**
+     * Set the shape color.
+     *
+     * @param color the new color
+     */
     public final void setColor(final String color) {
-        this.color = color;
+        this.myColor = color;
     }
 
+    /**
+     * Get the shape color.
+     *
+     * @return the color
+     */
     public final String getColor() {
-        return color;
+        return myColor;
     }
 }
